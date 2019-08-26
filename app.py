1c6f91bcdce36e4cd730e56478ec0aaf58306067
jacklu97 <jacklu.hdz@gmail.com>
2019-08-26 09:37:31 -0500

    Change of requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, make_response
-#from flask_basicauth import BasicAuth
+from flask_basicauth import BasicAuth
 from flask_marshmallow import Marshmallow
 import os
 from functools import wraps
@@ -22,10 +22,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['BASIC_AUTH_USERNAME'] = 'system'
-#app.config['BASIC_AUTH_PASSWORD'] = 'Sys1638'
-
-#basic_auth = BasicAuth(app)
+app.config['BASIC_AUTH_USERNAME'] = 'system'
+app.config['BASIC_AUTH_PASSWORD'] = 'Sys1638'
 
 # Inicializar db
 db.init_app(app)
@@ -35,6 +33,7 @@ ma.init_app(app)
 with app.app_context():
     db.create_all()
 
+basic_auth = BasicAuth(app)
 
 # Iniciar el esquema
 embarque_esquema = EmbarqueEsquema()
@@ -45,7 +44,7 @@ factura_esquema = FacturaEsquema()
 facturas_esquema = FacturaEsquema(many= True)
 
 @app.route('/')
-#@basic_auth.required
+@basic_auth.required
 def index():
     return '<h1>Hello world</h1>'
 
