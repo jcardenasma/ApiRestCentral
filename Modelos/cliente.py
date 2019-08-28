cfbc8d31a21cfe4ee1dd9dd4261601e9720954e9
jacklu97 <jacklu.hdz@gmail.com>
2019-08-28 10:23:29 -0500

    Unique client

--- Modelos/cliente.py
+++ Modelos/cliente.py
@@ -4,7 +4,7 @@ class Cliente(db.Model):
     idCliente = db.Column(db.Integer, primary_key = True)
     rfc = db.Column(db.String)
     password = db.Column(db.String)
-    crm = db.Column(db.String)
+    crm = db.Column(db.String, unique=True)
     embarques = db.relationship('Embarque')
     
     def __init__(self, rfc, passw, crm):
